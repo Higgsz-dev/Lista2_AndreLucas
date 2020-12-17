4061c120f6f2753b00fd5f0bc28ec09bb5215cf9
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:44:03 -0300

    Adicionando a pasta da questão 6

---
+++ Q6/Q6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o valor aplicado na poupança: ");
+                double valorIncial = double.Parse(Console.ReadLine());
+
+                double valorFinal = (valorIncial / 100 * 2) + valorIncial;
+
+                Console.WriteLine("O valor no final é de: "+valorFinal+" R$");
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
