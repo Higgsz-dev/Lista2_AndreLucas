4f5adf37e5512489f41e9574e8f2d712277bf53d
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:56:30 -0300

    Adicionando a pasta da questão12

---
+++ Q12/Q12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Q12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o primeiro numero: ");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Digite o primeiro numero: ");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("O valor da divisão do priemiro pelo segundo é de: "+num1/num2);
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
