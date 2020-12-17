64c91b07658eb3dc8a938394336e005e14581f7d
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:53:22 -0300

    Adicionando a pasta da questão10

---
+++ Q10/Q10/Program.cs
@@ -0,0 +1,26 @@
+﻿ using System;
+
+namespace Q10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o preço do litro da gasolina: ");
+                double precoGasolina = double.Parse(Console.ReadLine());
+
+                Console.Write("Digite o valor que você deseja abastecer: ");
+                double valorColocado = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Quantidade de litros abastecido: "+valorColocado/precoGasolina);
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
