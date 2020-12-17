67a817667c4a6beb86b3a9afaa323679a677b34d
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:57:41 -0300

    Adicionando a pasta da questão13

---
+++ Q13/Q13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Q13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite seu peso em quilos: ");
+                double peso = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Peso convertido para gramas: "+peso*1000);
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            };
+        }
+    }
+}
