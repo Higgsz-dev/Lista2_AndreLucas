b28e58f1bf4346deea5e3155d564e709b188915d
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:45:44 -0300

    Adicionando a pasta da questão 7

---
+++ Q7/Q7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Q7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o valor do produto: ");
+                double valorProduto = double.Parse(Console.ReadLine());
+
+                Console.Write("Digite a quantidade de parcelas: ");
+                int parcelas = int.Parse(Console.ReadLine());
+
+                double valorParcela = valorProduto / parcelas;
+
+                Console.WriteLine("O valor de cada parcela será de: "+valorParcela+" R$");
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
