03e616f288595ad9ba484a2edf7512eefd187147
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:41:58 -0300

    Adicionando a pasta da questão 4

---
+++ Q4/Q4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o preço de um carro novo: ");
+                double precoCarro = double.Parse(Console.ReadLine());
+
+                double custoFabrica = (precoCarro / 100 * 73) - precoCarro;
+
+                Console.WriteLine("O preço do carro ao consumidor final é de: "+custoFabrica+" R$");
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
