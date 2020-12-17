9d58b5eb940e10514edd74c12d0f7e2845d5a826
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:55:01 -0300

    Adicionando a pasta da questão12

---
+++ Q11/Q11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Q11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o peso do prato montado pelo cliente: ");
+                double pesoPrato = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Preço a pagar: "+pesoPrato*12+" R$");
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
