a99cdf4b2ef0713460c7b479f46e13e31205dbb8
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 20:39:47 -0300

    Adicionando a pasta da questão 2

---
+++ Q2/Q2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o número total de eleitores do municipio: ");
+                int totalEleitores = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o total de votos brancos: ");
+                int brancos = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o total de votos nulos: ");
+                int nulos = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o total de votos válidos: ");
+                int validos = int.Parse(Console.ReadLine());
+
+                double totalBrancos = totalEleitores / 100 * brancos;
+                Console.WriteLine("O percentual de votos em brancos: "+totalBrancos+" %");
+
+                double totalNulos = totalEleitores / 100 * nulos;
+                Console.WriteLine("O percentual de votos em nulos: " + totalNulos + " %");
+
+                double totalValidos = totalEleitores / 100 * validos;
+                Console.WriteLine("O percentual de votos em brancos: " + totalValidos + " %");
+
+                Console.Write("Deseja realizar outro calculos: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
