9cd51f84865426e4ceb2d643019734e6444b13e7
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:47:10 -0300

    Adicionando a pasta da questão44

---
+++ Q44/Q44/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Q44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para compara strings usamos ==");
+            Console.WriteLine("Digite um nome: ");
+            string nome1 = Console.ReadLine();
+
+            Console.WriteLine("Digite um nome: ");
+            string nome2 = Console.ReadLine();
+
+
+            if (nome1 == nome2)
+            {
+                Console.WriteLine("Os nomes são iguais");
+            }
+            else
+            {
+                Console.WriteLine("Os nome são diferentes");
+            }
+        }
+    }
+}
