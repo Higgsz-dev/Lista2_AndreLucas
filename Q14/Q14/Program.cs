07fc1ae2c4bc4541ebb99f8b36b26468eb705e26
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 21:01:55 -0300

    Adicionando a pasta da questão14

---
+++ Q14/Q14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Q14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite um número: ");
+                double num = double.Parse(Console.ReadLine());                
+
+                if (num >= 100 && num <= 200)
+                {
+                    Console.WriteLine("O número está entre 100 e 200");
+                }else
+                {
+                    Console.WriteLine("O número não está entre 100 e 200");
+                }
+
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
