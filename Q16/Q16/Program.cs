96b7774d590891e0b1000899de20174a857f9889
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 21:24:40 -0300

    Adicionando a pasta da questão 16

---
+++ Q16/Q16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Q16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite a sua idade: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                if(idade < 18)
+                {
+                    Console.WriteLine("Menor de idade");
+                }if (idade >= 18 && idade <= 49)
+                {
+                    Console.WriteLine("Maior de idade");
+                } if (idade >= 50)
+                {
+                    Console.WriteLine("Idoso");
+                }
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
