80264aca286bfcc5aa3cf95e4e65c1eb8834d963
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:29:04 -0300

    Adicionando a pasta da questão32

---
+++ Q32/Q32/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Q32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                for (int i = 1; i < 1000; i++)
+                {
+                    double calc = i%5;
+                    if(calc == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
