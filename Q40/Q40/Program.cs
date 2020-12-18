39aba58c0fdec542f4a6a0c8dcc17e5b48fee456
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:42:38 -0300

    Adicionando a pasta da questão40

---
+++ Q40/Q40/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Q40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void DizOi()
+            {
+                Console.WriteLine("Oi");
+            }
+
+            DizOi();
+        }
+    }
+}
