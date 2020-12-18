23fd783dd3603c7c08d4792bee56c469295cc076
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:37:28 -0300

    Adicionando a pasta da questão36

---
+++ Q36/Q1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o seu nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite quantos anos você tem: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(nome + " você já viveu " + idade * 365);
+                Console.WriteLine("");
+
+                Console.Write("Realizar outro calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
