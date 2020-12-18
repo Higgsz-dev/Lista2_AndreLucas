0e5b0eadc34e3d76d34cce1a320851d153c54633
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:44:42 -0300

    Adicionando a pasta da questão41

---
+++ Q41/Q41/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q41
+{
+    class Somar
+    {
+        public int Num1;
+        public int Num2;
+
+        
+
+        public override string ToString()
+        {
+            return "\nPimeiro numero: "+Num1 +
+                "\n Segundo número: "+Num2;
+        }
+    }
+}
---
+++ Q41/Q41/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Q41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classes são parecidas com funções, que por sua vez nos economizam tempo. Mas dentro das classes");
+            Console.WriteLine("temos os objetos, que carregam varias e paremetros");
+            Console.WriteLine("exemplo de uma classe para exibir numeros: ");
+
+            Somar s = new Somar();
+
+            Console.Write("Digite um número: ");
+            s.Num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um número: ");
+            s.Num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(s);
+        }
+    }
+}
