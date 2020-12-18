585747c44c6485867b2cd096f4a6366eb77c5d39
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:23:51 -0300

    Adicionando a pasta da questão30

---
+++ Q30/Q30/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Q30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+            int idade = 1;
+
+            int contIdades = 0;
+            int contTotal = 0;
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                while(idade > 0)
+                {
+                    Console.WriteLine("Digite a sua idade: ");
+                    idade = int.Parse(Console.ReadLine());
+
+                    contTotal = contTotal + 1;
+                    contIdades = contIdades + 1;
+                }
+
+                Console.WriteLine("A média das idades digitadas é: "+contIdades/contTotal);
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
