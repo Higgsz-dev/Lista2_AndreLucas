8f6f7516e8192188fcd9d1e93fb41ca323b9117d
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 21:27:15 -0300

    Adicionando a pasta da questão17

---
+++ Q17/Q17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Q17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o seu nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("M para Masculino e F feminino: ");
+                string sexo = Console.ReadLine();
+
+                if (sexo == "M")
+                {
+                    Console.WriteLine(nome);
+                    Console.WriteLine("É Homem");
+                }
+                else
+                {
+                    Console.WriteLine(nome);
+                    Console.WriteLine("É Mulher");
+                }
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
