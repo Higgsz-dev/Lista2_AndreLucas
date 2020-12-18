9c555afbba3854030fe2591b47cbffa93f7f461c
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:31:46 -0300

    Adicionando a pasta da questão34

---
+++ Q34/Q34/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Q34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+            double saldo = 300;
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                Console.Write("Digite o valor que deseja sacar");
+                double saque = double.Parse(Console.ReadLine());
+
+                double result = saldo - saque;
+
+                Console.WriteLine("Saldo da conta: "+result);
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
