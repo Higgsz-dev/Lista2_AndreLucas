3dfc9a1a05f9fb1752a39e401e7653b5eb53b571
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:13:34 -0300

    Adicionando a pasta da questão22

---
+++ Q22/Q22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Q22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    Console.WriteLine("5 X "+i+" = "+5*i);
+                }
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
