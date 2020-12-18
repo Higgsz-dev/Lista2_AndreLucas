9e58761104ec92e381e15f4ca6636aebbe8ac09b
LAFSHC <71662736+LAFSHC@users.noreply.github.com>
2020-12-17 22:17:03 -0300

    Adicionando a pasta da questão24

---
+++ Q24/Q24/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Q24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao = "sim";
+            int menor18 = 0;
+            int mais18 = 0;
+
+            while (opcao == "sim" || opcao == "Sim" || opcao == "SIM")
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.Write("Digite a sua idade: ");
+                    int idade = int.Parse(Console.ReadLine());
+
+                    if (idade <= 18)
+                    {
+                        menor18 =+ 1;
+                    }else
+                    {
+                        mais18 = +1;
+                    }
+                }
+
+                Console.WriteLine("Pesosa com menos de 18: " + menor18);
+                Console.WriteLine("Pessoa com mais de 18: " + mais18);
+
+                Console.Write("Fazer um novo calculo: ");
+                opcao = Console.ReadLine();
+            }
+        }
+    }
+}
